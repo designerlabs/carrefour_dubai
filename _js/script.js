aa5304a35970f6b1f22c4020aff03d6a75fb289e
Sridhar Swaminathan <designerlabs@hotmail.com>
2018-01-17 01:03:10 +0800

    updated

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,57 @@
 $(function(){
- 
-})
+
+
+     $('#form-signin_v2').validate({
+        submit: {
+           
+            callback: {
+                
+                onSubmit: function (node, formData) {
+                    console.log('#' + node[0].id + ' has been submitted.');
+                    console.log(formData);
+                    document.getElementById(node[0].id).reset();
+                }
+            }
+        }
+    });
+
+    var submitIcon = $('.searchbox-icon');
+            var inputBox = $('.searchbox-input');
+            var searchBox = $('.searchbox');
+            var isOpen = false;
+            submitIcon.click(function(){
+                if(isOpen == false){
+                    searchBox.addClass('searchbox-open');
+                    inputBox.focus();
+                    isOpen = true;
+                } else {
+                    searchBox.removeClass('searchbox-open');
+                    inputBox.focusout();
+                    isOpen = false;
+                }
+            });  
+             submitIcon.mouseup(function(){
+                    return false;
+                });
+            searchBox.mouseup(function(){
+                    return false;
+                });
+            $(document).mouseup(function(){
+                    if(isOpen == true){
+                        $('.searchbox-icon').css('display','block');
+                        submitIcon.click();
+                    }
+                    $(".searchbox-input").val('');
+                });
+});
+
+function buttonClk(){
+    var inputVal = $('.searchbox-input').val();
+    inputVal = $.trim(inputVal).length;
+    if( inputVal !== 0){
+        $('.searchbox-icon').css('display','none');
+    } else {
+        $('.searchbox-input').val('');
+        $('.searchbox-icon').css('display','block');
+    }
+}
